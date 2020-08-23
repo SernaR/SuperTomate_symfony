afff5a425f456f478c3f6c1ebc55079de47a050f
SernaR <rserna@outlook.fr>
2020-08-23 14:17:36 +0200

    production configuration

--- assets/js/services/config.js
+++ assets/js/services/config.js
@@ -1,4 +1,4 @@
-export const URL = 'https://localhost:8000/'
-
+export const URL = process.env.APP_URL
+//export const URL = 'https://localhost:8000/'
 export const ADMIN_API = URL + 'administrateur/'
 export const USER_API = URL + 'utilisateur/'
